c107501778021b72597f5fb37327c6f734563d87
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2021-11-29 11:50:45 -0600

    v0.0.1 setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
+import { firebaseConfig } from "./firebase.config";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -5,15 +7,7 @@
 export const environment = {
   production: false,
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  firebase : {
-    apiKey: 'AIzaSyBDT-vR9srRABPTD6CL6MH38eHUWwvBFSE',
-    authDomain: 'kanbanfire-219.firebaseapp.com',
-    projectId: 'kanbanfire-219',
-    storageBucket: 'kanbanfire-219.appspot.com',
-    messagingSenderId: '978923615970',
-    appId: '1:978923615970:web:c64c82dd29f140208ed0bb',
-    measurementId: 'G-Q1MJ6F7RSB'
-  }
+  firebase : firebaseConfig
 };
 
 /*
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBDT-vR9srRABPTD6CL6MH38eHUWwvBFSE',
+    authDomain: 'kanbanfire-219.firebaseapp.com',
+    projectId: 'kanbanfire-219',
+    storageBucket: 'kanbanfire-219.appspot.com',
+    messagingSenderId: '978923615970',
+    appId: '1:978923615970:web:c64c82dd29f140208ed0bb',
+    measurementId: 'G-Q1MJ6F7RSB'
+}
